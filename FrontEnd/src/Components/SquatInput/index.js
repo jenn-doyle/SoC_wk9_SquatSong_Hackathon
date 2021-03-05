c3aa92aa08d4f7b3853d02f30aafcc4134a9dfe1
Nathan Hinks <nathanhinks.dev@gmail.com>
2021-03-05 11:28:12 +0000

    SquatInput component done

--- FrontEnd/src/Components/SquatInput/index.js
+++ FrontEnd/src/Components/SquatInput/index.js
@@ -1,7 +1,31 @@
+import { useState } from 'react';
 import './SquatInput.css';
 
+//takes in function to send squats back up to app.
 function SquatInput() {
-  return ;
+	const [ input, setInput ] = useState(5);
+
+	return (
+		<form className='squat-input-form'>
+			<input
+				className='squat-input-number'
+				type='number'
+				min='0'
+				value={input}
+				onChange={(e) => setInput(e.target.value)}
+			/>
+			<button
+				className='squat-input-btn'
+				type='submit'
+				onClick={(e) => {
+          e.preventDefault();
+					//send data up
+				}}
+			>
+				Get Songs
+			</button>
+		</form>
+	);
 }
 
 export default SquatInput;
