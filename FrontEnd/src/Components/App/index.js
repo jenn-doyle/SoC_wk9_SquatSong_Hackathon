d17e3ad8a9a7322bd4750c332e56ff55b67e9cb4
Nathan Hinks <nathanhinks.dev@gmail.com>
2021-03-05 12:04:57 +0000

    Song List done

--- FrontEnd/src/Components/App/index.js
+++ FrontEnd/src/Components/App/index.js
@@ -1,11 +1,12 @@
 import "./App.css";
 import SquatInput from "../SquatInput";
 import AddSongInput from "../AddSongInput";
+import SongList from '../SongList';
 
 function App() {
   return (
     <div className="App">
-      <AddSongInput />
+      <SongList></SongList>
     </div>
   );
 }
--- FrontEnd/src/Components/SongList/index.js
+++ FrontEnd/src/Components/SongList/index.js
@@ -1,7 +1,32 @@
 import './SongList.css';
 
+//example data!!!
+const initialData = [
+	{
+		title    : 'bob',
+		artist   : 'bob',
+		duration : 'bob',
+		link     : 'bob',
+		yourName : 'bob',
+	},
+];
+
+//takes in data object
 function SongList() {
-  return 
+	return (
+		<ul className='song-list'>
+			{initialData.map((item) => (
+				<li className='song-list-item'>
+					<span className='title-artist'>
+						{item.title} - {item.artist}
+					</span>
+					<span className='duration'>{item.duration}</span>
+					<span className='link'>{item.link}</span>
+					<span className='recommended-by'>Recommended by:{item.yourName}</span>
+				</li>
+			))}
+		</ul>
+	);
 }
 
 export default SongList;
