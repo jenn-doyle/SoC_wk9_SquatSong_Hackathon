97ee23ba5a0d446b37f7d0077b4217741ffda878
Jenn <jenndoyle@hotmail.co.uk>
2021-03-05 18:08:17 +0000

    testes

--- UnitTest/UnitTest1.cs
+++ UnitTest/UnitTest1.cs
@@ -10,17 +10,27 @@ public class SongControllerTests
     private readonly Mock<IRepository<MoQuotePlusSong>> _mockRepo;
     private readonly SongController _controller;
 
-    public BookControllerTests()
+    public SongControllerTests()
     {
         _mockRepo = new Mock<IRepository<MoQuotePlusSong>>();
         _controller = new SongController(_mockRepo.Object);
     }
 
     [Fact]
-    public void GetbySlc_WhenIdCalled_ReturnsASong()
+    public void GetbySlc_WhenSlcCalled_ReturnsASong()
     {
         var songResult = _controller.Get(3);
 
         Assert.IsType<Task<IActionResult>>(songResult);
+    }
+
+    [Fact]
+    public async Task GetbySlc_WhenSlcIsPassed_ReturnsOkResult()
+    {
+        var slcResult = await _controller.Get(2);
+        var actual = slcResult as OkObjectResult;
+
+        Assert.NotNull(actual);
+        Assert.Equal(200, actual.StatusCode);
     }
 }
