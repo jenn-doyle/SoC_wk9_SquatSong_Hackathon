b867fe3bae138af7ba39c05941b0e7a55bb7cbd3
Nathan Hinks <nathanhinks.dev@gmail.com>
2021-03-05 12:09:55 +0000

    App Components structured

--- FrontEnd/src/Components/App/index.js
+++ FrontEnd/src/Components/App/index.js
@@ -1,14 +1,28 @@
-import "./App.css";
-import SquatInput from "../SquatInput";
-import AddSongInput from "../AddSongInput";
+import AddSongInput from '../AddSongInput';
+import QuoteDisplay from '../QuoteDisplay';
 import SongList from '../SongList';
+import SquatInput from '../SquatInput';
+import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <SongList></SongList>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<h1>
+				{' '}
+				<span>SSSG</span> (Super Squat Song Generator)
+			</h1>
+			<div className='left-side'>
+				<SquatInput />
+				<QuoteDisplay />
+			</div>
+			<div className='right-side'>
+				<SongList />
+			</div>
+			<div className='footer-form'>
+				<AddSongInput />
+			</div>
+		</div>
+	);
 }
 
 export default App;
