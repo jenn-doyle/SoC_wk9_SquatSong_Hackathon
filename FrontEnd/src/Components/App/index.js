8d8f015a6f3c3672644f1ff848836d466930cbd4
Jenn <jenndoyle@hotmail.co.uk>
2021-03-05 21:06:45 +0000

    css

--- FrontEnd/src/Components/App/index.js
+++ FrontEnd/src/Components/App/index.js
@@ -17,17 +17,23 @@ function App() {
   return (
     <div className="App">
       <h1>
-        <span>SSSG</span> (Super-Squat Song Generator)
+        <span id="title">S-S-S-G</span> 
+        <br/>
+        (Super Squat Song Generator)
       </h1>
+      <img id="man" src ="https://hips.hearstapps.com/ame-prod-menshealth-assets.s3.amazonaws.com/main/assets/bodyweight-squat.gif?resize=480:*" alt = "man sqautting"/>
+      <img id="woman" src ="https://thumbs.gfycat.com/BlueWetHarvestmouse-small.gif" alt = "woman sqautting"/>
       <h2>Enter How Many Squats You Gotta Do... </h2>
-      <p></p>
       <SquatInput className="squat-input-com" onClick={setDataState} />
       <QuoteDisplay className="quote-display-com" data = {appData}/>
-
       <SongList className="list-com" data ={appData} />
-
       <AddSongInput className="add-song-com" />
+      
+      <img id="world" src ="https://d33wubrfki0l68.cloudfront.net/d6e8999f4d43f37bbc7b46cbccd9bffdc9ffdf26/62323/static/planet_soc-936d90fa66f241adff76225618c37d0f.png" alt = "soc world"/>
+      <img id="soc" src ="https://www.schoolofcode.co.uk/static/logo-51c754388b198e5bbb0d08a971ebbfa2.png" alt = "soc logo"/>
+      
     </div>
+    
   );
 }
 
