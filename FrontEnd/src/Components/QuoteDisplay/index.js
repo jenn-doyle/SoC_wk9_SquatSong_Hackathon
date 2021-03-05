4c7bc6931f47bb60f9e343e72797f3d56e6d1c1e
Liam Morley <liammorley123@live.co.uk>
2021-03-05 12:02:19 +0000

    add quote component

--- FrontEnd/src/Components/QuoteDisplay/index.js
+++ FrontEnd/src/Components/QuoteDisplay/index.js
@@ -1,7 +1,13 @@
-import './QuoteDisplay.css';
+import "./QuoteDisplay.css";
 
-function QuoteDisplay() {
-  return ;
+function QuoteDisplay({ quote, person }) {
+  // take in data from fetch
+  return (
+    <div>
+      <h1>{quote}</h1>
+      <h3>{person}</h3>
+    </div>
+  );
 }
 
 export default QuoteDisplay;
